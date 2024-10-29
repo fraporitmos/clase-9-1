12a2545956ff39bbff305169965f46a1312ec89c
Fran Espino <fraporitmos>
2024-10-28 21:57:41 -0500

    fundamentos

---
+++ ejercicio3/app.js
@@ -0,0 +1,10 @@
+/* 
+Titulo: Juego del numero clave
+
+Descripción: 
+Define un numero clave entre 0 y 20, luego deberás
+pedirle al usuario un numero menor a 20 y el usuario deberá 
+de adivinar si el numero ingresado es igual al numero establecido 
+como numero clave y mostrarle un mensaje que diga "Ganaste, adivinaste el numero clave" de caso contrario mostrar el mesnaje  "No es el numero, vuelve a intentarlo".
+*/
+
