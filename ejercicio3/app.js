9c867425e8b334d92f51b3de462d501f7d161181
Fran Espino <fraporitmos>
2024-10-30 20:04:32 -0500

    Ejercicio 3

--- ejercicio3/app.js
+++ ejercicio3/app.js
@@ -8,3 +8,23 @@ de adivinar si el numero ingresado es igual al numero establecido
 como numero clave y mostrarle un mensaje que diga "Ganaste, adivinaste el numero clave" de caso contrario mostrar el mesnaje  "No es el numero, vuelve a intentarlo".
 */
 
+let numero = 8
+let i = 0
+const MAX_INTENTOS = 4
+
+while(i < MAX_INTENTOS){
+    i = i + 1
+    let adivina = parseInt(prompt("Ingresa el numero clave"))
+    if(adivina == numero){
+        alert(`Ganaste, encontraste el numero clave: ${numero}\nNum. Intentos: ${i}`)
+        break
+    }else{
+        alert("No es el numero clave, intenta nuevamente.")
+    }
+}
+
+if(i === MAX_INTENTOS){
+    alert("Maximo de intentos cumplidos")
+}
+
+
